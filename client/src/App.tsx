4612a14fc966cc07c898701dce3e36a3b3c7c030
Claudio <claudio.98.itra@gmail.com>
2021-01-26 12:24:15 +0100

    Add main interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { SetStateAction, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import { ETIMEDOUT } from 'constants';
 
-const state = {listItems: []};
+// const state = {listItem: []};
+
+interface Person {
+  id: number;
+  name: string;
+  email: string;
+  treatment: string;  
+}
 
 function App() {
-  const [listItems, setListItems] = React.useState([])
+  const [itemList, setItemList] = React.useState<Person[]>([])
+  const [edition, setEdition] = React.useState<Boolean>(false)
 
-  function callAPI() {
+  function callApi() {
     return fetch("http://localhost:9000")
         .then(res => res.json())
-        .then(res => {
-          res.map((patient: any) => console.log(patient))
-        })
+        .then(res => res as Person[])
+        .then(res => setItemList(res))
   }
 
-  callAPI();
+  useEffect(() => {
+    callApi()
+  }, [])
+
+  function toggleEdition() {
+    setEdition(!edition)
+  }
 
   return (
   <>
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Treatment</th>
-        </tr>
-      </thead>
-      <tbody>
-        {listItems.map((value, index) => {
-          return (
-            <tr>
-              <td>{index}</td>
-              {/* <td>{value.name}</td>
-              <td>{value.email}</td>
-              <td>{value.treatment}</td> */}
-            </tr>
-          )
-        })}
-      </tbody>
-    </Table>
-    <p>s</p>
+    <Container className="mt-5">
+      <Button variant="success" className="mb-3">New registry</Button>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Treatment</th>
+            <th>Actions</th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{itemList[0] && itemList[0].id}</td>
+            <td>{itemList[0] && itemList[0].name}</td>
+            <td>{itemList[0] && itemList[0].email}</td>
+            <td>{itemList[0] && itemList[0].treatment}</td>
+            <td>
+              <ButtonGroup className="d-flex">
+                <Button variant="outline-primary">Edit</Button>
+                <Button variant="outline-danger">Delete</Button>
+              </ButtonGroup>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
   </>
 
   );
